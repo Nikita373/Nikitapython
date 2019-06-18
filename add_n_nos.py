f40a17314b394a81ec2fb603254afb683ab5ee8e
Nikita373 <51940717+Nikita373@users.noreply.github.com>
2019-06-18 11:00:17 +0530

    Add files via upload
    Addition of n nos using lists

---
+++ add_n_nos.py
@@ -0,0 +1,17 @@
+a=list(input("Enter the nos with space in between").split())
+i=0
+addn=0
+for i in range(len( a)):
+         a[i]=int(a[i])
+print(a)
+
+'''for  i in  range(len(a)):
+         addn=addn+a[i]
+
+print(addn)'''
+print(sum(a))
+
+
+         
+
+         
